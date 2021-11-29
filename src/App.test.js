5ee2a7a534a5f73955ce80827930dedb84217bce
Daniil Yuzhanin <Daniil_Iuzhanin@epam.com>
2021-11-29 13:01:28 +0300

    init: start task in 13.00

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders home page', () => {
+  const { container } = render(
+    <App />
+  );
+  expect(container).toMatchSnapshot();
+});
